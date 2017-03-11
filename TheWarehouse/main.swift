f148b9409560f74ca706c4e75b86b7d7e372e04b
rsampdev <rsampdev@gmail.com>
2017-03-11 16:29:37 -0600

    Initial Commit

---
+++ TheWarehouse/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TheWarehouse
+//
+//  Created by Rodney Sampson on 3/11/17.
+//  Copyright © 2017 com.rsampdev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
